0eb7e36901fec6cdadff557c07a30ddafcb3d5f0
jeongwoo0427 <jwk9022648@gmail.com>
2025-11-27 12:56:02 +0900

    feat: 기본 설정파일 구성

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map({ "n", "i", "v", "t" }, ";j", "<C-[>")
+map({ "n", "v" }, "<leader>j", "30j", { desc = "Move bottom 30" })
+map({ "n", "v" }, "<leader>k", "30k", { desc = "Move top 30" })
+map({ "n", "v" }, "T", ":Bwipeout<CR>", { remap = true, desc = "Close buffer (Lsp safe)" }) -- 필수: "famiu/bufdelete.nvim" 설치 필요
+map({ "n", "v" }, "<leader>t", "<C-/>", { remap = true, desc = "Terminal" })
+map({ "n", "v" }, "<leader>l", vim.lsp.buf.format, { remap = true, desc = "Code Format" })
+map({ "n", "i", "v" }, "<leader><Enter>", vim.lsp.buf.code_action, { remap = true, desc = "Code Action" })
+map({ "i" }, "<leader>.", vim.lsp.buf.completion, { remap = true, desc = "Trigger completion" })
+map({ "n" }, "<leader>.", vim.lsp.buf.signature_help, { remap = true, desc = "Signature help" })
+map({ "n" }, "<leader>h", vim.lsp.buf.hover, { remap = true, desc = "Hover" })
+map({ "n" }, "<leader>r", vim.lsp.buf.rename, { remap = true, desc = "Rename" })
+map({ "n", "v" }, "<leader><leader>", "l", { remap = true, desc = "NONE"})
+
+-- 여기에 d와 x가 클립보드에 복사되지 않도록 추가된 설정
+map({ "n", "v" }, "d", '"_d', { desc = "Delete without yanking" })
+map({ "n" }, "x", '"_x', { desc = "Delete character without yanking" })
+map({ "n", "v" }, "D", '"_D', { desc = "Delete to end of line without yanking" })
+
+-- 잘라내기는 오르직 이것 하나로
+map({ "v" }, "x", "d", { desc = "Cut" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.autoformat = false -- 수정 마친 후 자동 정렬 활성 여부
+vim.o.exrc = true -- 프로젝트별.nvim.lua 자동 실행 여부 
+vim.o.secure = true -- .nvim.lua 안의 위험한 명령어 (:!, :luafile) 자동 차단 여부
+
