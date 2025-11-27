6ca765db455d88e9ce8aa7f32a5983a3ec3af798
jw.kim <jw.kim@smartro.co.kr>
2025-11-27 13:15:39 +0900

    feat: 마우스 클릭 방지 추가

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,5 @@
 vim.g.autoformat = false -- 수정 마친 후 자동 정렬 활성 여부
 vim.o.exrc = true -- 프로젝트별.nvim.lua 자동 실행 여부 
 vim.o.secure = true -- .nvim.lua 안의 위험한 명령어 (:!, :luafile) 자동 차단 여부
+vim.o.mouse = ""
 
