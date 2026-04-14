7743b5a12d96bff46fc4c45ca04c527e63e7fbcf
dogfoot <64342840+jeongwoo0427@users.noreply.github.com>
2026-04-14 10:39:19 +0900

    Revert "fix: python plugin setting"
    This reverts commit 17ad2d4abccc27ce34f4555e2f6f21383f6020f4.

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,19 +1,3 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pyright = {
-          before_init = function(_, config)
-            local venv = vim.fn.getcwd() .. "/.venv/bin/python"
-            if vim.fn.executable(venv) == 1 then
-              config.settings = config.settings or {}
-              config.settings.python = config.settings.python or {}
-              config.settings.python.pythonPath = venv
-            end
-          end,
-        },
-      },
-    },
-  },
+  { import = "lazyvim.plugins.extras.lang.python" },
 }
