17ad2d4abccc27ce34f4555e2f6f21383f6020f4
dogfoot <64342840+jeongwoo0427@users.noreply.github.com>
2026-04-14 10:29:37 +0900

    fix: python plugin setting

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,3 +1,19 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.python" },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          before_init = function(_, config)
+            local venv = vim.fn.getcwd() .. "/.venv/bin/python"
+            if vim.fn.executable(venv) == 1 then
+              config.settings = config.settings or {}
+              config.settings.python = config.settings.python or {}
+              config.settings.python.pythonPath = venv
+            end
+          end,
+        },
+      },
+    },
+  },
 }
